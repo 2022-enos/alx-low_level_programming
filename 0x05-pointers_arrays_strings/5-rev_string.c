261ec8c1c150b490866b3b03a1f374d6183ebc31
2022-enos <azarenos@yahoo.com>
2022-09-25 09:59:13 -0700

    task 5.0

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,7 @@
 void rev_string(char *s)
 {
 	int i, j, k, temp;
-	
+
 	i = 0;
 	while (s[i] != '\0')
 	{
@@ -16,7 +16,7 @@ void rev_string(char *s)
 	k = 0;
 	j = i - 1;
 
-	while (k<j)
+	while (k < j)
 	{
 		temp = s[k];
 		s[k] = s[j];
