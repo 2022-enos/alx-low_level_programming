d0ffeca1d69e412f3f3dbcde50603d8cca5b37bc
2022-enos <azarenos@yahoo.com>
2022-09-25 15:42:01 -0700

    task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
-#define MAIN.H
+#define MAIN_H
 
 
-int _putchar(char c)
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -10,4 +10,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 
 
-#endif /* MAIN_H 
+#endif
